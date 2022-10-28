f0c09616ad5ddda3371899c95a9f88a8a3283150
yesimzaim <yesimzaim@gmail.com>
2022-10-28 15:24:31 +0200

    committing final version

---
+++ ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_memmove.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yzaim <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/05 17:04:51 by yzaim         #+#    #+#                 */
+/*   Updated: 2022/10/18 12:49:42 by yzaim         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*b_dst;
+	char	*b_src;
+
+	if (!dst && !src)
+		return (NULL);
+	b_src = (char *)src;
+	b_dst = (char *)dst;
+	if (src < dst)
+	{
+		b_src += len - 1;
+		b_dst += len - 1;
+		while (len --)
+			*b_dst-- = *b_src--;
+	}
+	else
+	{	
+		while (len--)
+		{
+			*b_dst++ = *b_src++;
+		}
+	}
+	return ((void *)dst);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,8 +6,11 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdarg.h>
+#include <string.h>
 
-
+char	*ft_itoa(int n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 //how it works:
 //iteratire over the string, only stopping at %
 //that when you know you have a value that you have to convert to a string
@@ -16,24 +19,32 @@
 //%x and %X are the same - one CAPS other not.(base16)
 //%u - unsigned decimal(base10)
 //single % sign is ignored! to print \ or % you have to double them.
-int main ()
+/*int main ()
 {
 	printf("d: %d\ni: %i\nu: %u\nx: %x\nX:%X\n", (-(INT_MIN)-2),(-( INT_MIN)-2), (-(INT_MIN)-2), (-(INT_MIN)-2), (-(INT_MIN)-2));
 	printf("what does the \\ mean %%\n");
 
-}
+}*/
+
+/*int	parse_string(char *str, char *dst)
+{
+
+}*/
 
 int	ft_printf(const char *str, ...)
 {
-	va_list	arg;
-	int	i;
-	int	j;
-	int	arg_num;
-	char	buf[400] = {0};
+	va_list		args;
+	int		i;
+	int		j;
+	int		arg_num;
+	char		arg_char;
+	char		*store;
+	char		buf[400] = {0};
 	
-	va_start(arg, str);
+	va_start(args, str);
 	i = 0;
 	j = 0;
+	arg_num = 0;
 	while (str && str[i])
 	{
 		if (str[i] != '%')
@@ -44,13 +55,42 @@ int	ft_printf(const char *str, ...)
 		else
 		{
 			i++;
+			if (str[i] == 'c')
+			{
+				//arg_num = va_arg(args, int);
+				buf[j] = va_arg(args, int);
+				//printf("char: %c\n", arg_num);
+			}
+			else if (str[i] == 'd')
+			{
+				arg_num = va_arg(args, int);
+				store = ft_itoa(arg_num);
+				ft_strlcpy(&buf[j], store, 300);
+				j = j + (strlen(store));
+				//arg_num = va_arg(args, int);
+				//printf("arg_nums %s\n", store);
+			}
+			else if (str[i] == 's')
+			{
+				store = (char *)va_arg(args, const char *);
+				ft_strlcpy(&buf[j], store, 300);
+				j = j + (strlen(store));
+				//printf("arg: %s\n", store);
+			}
+			j++;
+		}
+	//	else
+		/*{
+			i++;
 			if (str[i] = 'c')
 			{
-				arg_num += va_arg(arg, str);
-				buf[j] = str[arg_num];
+				arg_num = va_arg(args, char);
+				buf[j] = arg_num;
 			}
 			else if (str[i] == 'd' || str[i] == 'i')
 			{
+				arg_num = va_arg(args, int);
+				buf[j] = arg_num;
 			}
 			else if (str[i] == 'u')
 			{
@@ -64,12 +104,16 @@ int	ft_printf(const char *str, ...)
 			else if (str[i] == '%')
 				buf[j] = '%';
 			j++;
-		}
-	
-
-
+		}*/
 		i++;
 	}
+	va_end(args);
+	printf("buf is: %s\n", buf);
 	
-	return (NULL);
+	return (0);
+}
+
+int	main(void)
+{
+	ft_printf("here is my string %d, %s", 30, "string");;
 }
